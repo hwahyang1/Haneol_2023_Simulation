dc12606f1acae38a703b589760c1f6bb108cc71f
chick_0 <chick_0@ch1ck.xyz>
2023-05-27 22:48:10 +0900

    fix: Camera rotation issue
    Use `Mathf.Clamp` to fix issue and use `eulerAngles` instead of `Transform.rotation`

--- Assets/Scripts/Stage_A/PlayerController.cs
+++ Assets/Scripts/Stage_A/PlayerController.cs
@@ -7,13 +7,6 @@ namespace Stage_A
 		[Header("Config")]
 		[SerializeField]
 		private float moveSpeed;
-		[SerializeField]
-		private float rotationSpeed;
-
-		public float RotationSpeed
-		{
-			get { return rotationSpeed; }
-		}
 
 		private Rigidbody rb;
 
@@ -26,12 +19,6 @@ namespace Stage_A
 
 		private void Update()
 		{
-			PlayerMove();
-			PlayerRotate();
-		}
-
-		private void PlayerMove()
-		{
 			float h = Input.GetAxis("Horizontal");
 			float v = Input.GetAxis("Vertical");
 			float power = moveSpeed * Time.deltaTime;
@@ -39,13 +26,6 @@ namespace Stage_A
 			Vector3 moveDir = new Vector3(h * power, 0f, v * power);
 
 			rb.MovePosition(rb.position + transform.TransformDirection(moveDir));
-		}
-
-		private void PlayerRotate()
-		{
-			float mouseX = Input.GetAxisRaw("Mouse X");
-
-			rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, mouseX * rotationSpeed, 0f));
 		}
 	}
 }
